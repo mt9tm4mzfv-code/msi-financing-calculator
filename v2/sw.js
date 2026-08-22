9d5ce513ff70dc07a28c895d3f8f033926ad5d1f
mt9tm4mzfv-code <mt9tm4mzfv@privaterelay.appleid.com>
2026-08-22 20:39:10 +0800

    Add MSI Calculator V2 service worker

---
+++ v2/sw.js
@@ -0,0 +1,12 @@
+const CACHE="msi-financial-calculator-v2";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./sw.js","../access-guard.js"];
+self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()).catch(()=>{})));
+self.addEventListener("activate",event=>event.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET")return;
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+    const copy=response.clone();
+    if(new URL(event.request.url).origin===location.origin){caches.open(CACHE).then(cache=>cache.put(event.request,copy));}
+    return response;
+  }).catch(()=>cached)));
+});
